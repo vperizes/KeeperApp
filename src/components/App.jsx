6b3ba02948696453ccc0d73502171fa1fe6f33bb
vperizes <vperizes@gmail.com>
2023-10-16 22:59:13 -0500

    Simplify code by using arrow function in data mapping method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,20 @@ import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
 import notes from "../notes.js";
 
-function addNote(note) {
-    return (
-        <Note
-            key={note.id}
-            noteTitle={note.title}
-            noteContent={note.content}
-        />
-    );
-}
 
 function App() {
     return (
         <div>
             <Header />
-            {notes.map(addNote)}
+            {notes.map( note => {
+                return (
+                    <Note
+                        key={note.id}
+                        noteTitle={note.title}
+                        noteContent={note.content}
+                    />
+                );
+            })}
             <Footer />
         </div>
     );
