7f59d5ab1fcdcbb31255c02048d65bbb786af59e
vperizes <vperizes@gmail.com>
2023-10-23 23:53:38 -0500

    Implement functionality to add note based on user input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
@@ -6,11 +6,27 @@ import CreateArea from "./CreateArea.jsx";
 
 
 function App() {
+    //array where we keep all notes
+    const[allNotes, setNotes] = useState([]);
+
+    //we pass over "note" from create area component using props
+    function addNote(note){
+        setNotes((prevNotes) => {
+            return[...prevNotes, note];
+        });
+
+        console.log(allNotes);
+    }
+
     return (
         <div>
             <Header />
-            <CreateArea />
-            <Note />
+            <CreateArea create={addNote}/>
+            {allNotes.map((note, index) => {
+                return(
+                    <Note key={index} id={index} noteTitle={note.title} noteContent={note.noteBody}/>  
+                )
+            })}
             <Footer />
         </div>
     );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+    //creating an object that tracks the state of the input to make a note
+    const[note, setNote] = useState({
+        title: "",
+        noteBody:""
+    });
+
+    function updateNote(event){
+        const inputName = event.target.name;
+        const inputValue = event.target.value;
+
+        setNote((prevNote) => {
+            if(inputName === "title"){
+                return{
+                    title: inputValue,
+                    noteBody:prevNote.noteBody
+                };
+            } else if(inputName === "content"){
+                return{
+                    title: prevNote.title,
+                    noteBody: inputValue
+                };
+            }
+        });
+
+    }
+
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={updateNote} name="title" placeholder="Title" value={note.title}/>
+        <textarea onChange={updateNote} name="content" placeholder="Take a note..." rows="3" value={note.noteBody}/>
+        <button onClick={(event) => {
+            event.preventDefault();
+            props.create(note);
+            setNote({
+                title: "",
+                noteBody: ""
+            });
+        }}>Add</button>
       </form>
     </div>
   );
